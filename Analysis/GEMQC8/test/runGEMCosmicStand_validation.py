fa9f2e96f897fd3f8c9e996089a26c4fb7f4f51c
Byeonghak Ko <quark2930@hotmail.com>
2019-05-20 20:08:38 +0900

    Some customizing

--- Analysis/GEMQC8/test/runGEMCosmicStand_validation.py
+++ Analysis/GEMQC8/test/runGEMCosmicStand_validation.py
@@ -109,15 +109,19 @@ for i in range(6-len(str(run_number))):
   fpath = fpath + '0'
 fpath = fpath + str(run_number) + "/"
 
+#strSrcFile = "run000090_Cosmic_CERNQC8_2019-04-29_chunk_72.dat"
+strSrcFile = "run000101_Cosmic_CERNQC8_2019-05-07_chunk_75.dat"
+run_number = int(strSrcFile.split("_")[ 0 ].replace("run", "")) + 20000
 # Input source
 process.source = cms.Source(
                             "GEMLocalModeDataSource",
-                            fileNames = cms.untracked.vstring ([fpath+x for x in os.listdir(fpath) if x.endswith(".dat")]),
+                            #fileNames = cms.untracked.vstring ([fpath+x for x in os.listdir(fpath) if x.endswith(".dat")]),
                             skipEvents=cms.untracked.uint32(0),
                             fedId = cms.untracked.int32(1472),  # which fedID to assign
                             hasFerolHeader = cms.untracked.bool(False),
                             runNumber = cms.untracked.int32(run_number),
                             )
+process.source.fileNames = cms.untracked.vstring (["file:" + strSrcFile])
 														
 process.options = cms.untracked.PSet(
                                      SkipEvent = cms.untracked.vstring('ProductNotFound')
@@ -129,7 +133,9 @@ CondDB.DBParameters.authenticationPath = cms.untracked.string('/afs/cern.ch/cms/
 
 #eMapFile = 'GEMeMap_'+colType[0]+colType[1]+colType[2]+'.db'
 
-eMapFile = 'GEMeMap.db'
+#eMapFile = 'GEMeMap.db'
+eMapFile = 'GEMeMap_SSS.db'
+#eMapFile = 'GEMeMap_gemsw.db'
 
 CondDB.connect = cms.string('sqlite_fip:Analysis/GEMQC8/data/EMapFiles/'+eMapFile)
 
@@ -137,14 +143,14 @@ process.GEMCabling = cms.ESSource("PoolDBESSource",
                                   CondDB,
                                   toGet = cms.VPSet(cms.PSet(
                                                              record = cms.string('GEMeMapRcd'),
-																														 tag = cms.string('GEMeMap_v3')
-                                                             #tag = cms.string('GEMeMap_v6')
+																														 #tag = cms.string('GEMeMap_v3')
+                                                             tag = cms.string('GEMeMap_v6')
                                                              )
                                                     )
                                   )
 ####################################
 
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+#process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 # validation event filter
 process.load('EventFilter.L1TRawToDigi.validationEventFilter_cfi')
 
