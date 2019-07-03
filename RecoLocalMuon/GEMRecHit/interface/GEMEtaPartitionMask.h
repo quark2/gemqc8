558b444b37ba9a4690dea53bd06bcfdd89aa8fea
Giovanni Mocellin <gio.moc@gmail.com>
2019-07-03 18:57:49 +0200

    Introducing fixes Yechan did on the Hot strips masking To be remembered: check if strip numbering is from 1 or 0... If it is from 1 we need to write the masking file with a +1 somewhere!

---
+++ RecoLocalMuon/GEMRecHit/interface/GEMEtaPartitionMask.h
@@ -0,0 +1,10 @@
+#ifndef RecoLocalMuon_GEMRecHit_GEMEtaPartitionMask_h
+#define RecoLocalMuon_GEMRecHit_GEMEtaPartitionMask_h
+
+#include <bitset>
+#include <vector>
+
+const int maskSIZE=769;
+typedef std::bitset<maskSIZE> EtaPartitionMask;
+
+#endif
