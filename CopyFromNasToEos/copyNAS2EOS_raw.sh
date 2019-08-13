ff95ffd569b5500f166c3a85237920b74f215e15
Giovanni Mocellin <gio.moc@gmail.com>
2019-08-13 19:44:02 +0200

    Updated copy of raw files with cat of all index of the same lumi section

--- CopyFromNasToEos/copyNAS2EOS_raw.sh
+++ CopyFromNasToEos/copyNAS2EOS_raw.sh
@@ -4,19 +4,21 @@
 #
 # Created by: Kalpanie Madara Liyanage
 #
-# Usage: Copy QC8 uFEDKit run files from NAS to EOS
+# Usage: Copy QC8 uFEDkit run files from NAS to EOS
 
 RUN=$(printf "%06d" $1)
+LUMI=$(printf "%04d" $2)
 DIRECTORY=/eos/cms/store/group/dpg_gem/comm_gem/QC8_Commissioning/run$RUN
 src=/data/bigdisk/GEM-Data-Taking/GE11_QC8/Cosmics/run$RUN/run$RUN
 COUNT=0
 COPY=0
+size=0
 
 if [ ! -d "$DIRECTORY" ]; then
-	echo "Creating Directory: "$DIRECTORY
-	mkdir $DIRECTORY
+echo "Creating Directory: "$DIRECTORY
+mkdir $DIRECTORY
 else
-	echo "Existing Directory: "$DIRECTORY
+echo "Existing Directory: "$DIRECTORY
 fi
 
 #Get the list of files to send
@@ -26,36 +28,42 @@ FILES=$(ssh gemuser@gem904qc8daq ls ${src}"_ls"*".raw")
 echo "The list of files to be copied is:"
 for f in $FILES\;
 do
-	echo $f
-	((COUNT++))
+echo $f
 done
 
 MODTIME=$(ssh gemuser@gem904qc8daq date -r ${src}"_ls0001_index000000.raw" +"%Y-%m-%d_%H-%M-%S")
+#echo $MODTIME
+
+lumi=$(expr $2 + 0)
 
 #Copy the files
 echo "Copying files"
-for f in $FILES;
+
+for (( l=1; l<=$lumi; l++ ))
 do
-	echo "Processing " $f
-	ls=$(echo "$f" | cut -d'_' -f 3)
-	index=$(echo "$f" | cut -d'_' -f 4)
-	name=run$RUN"_Cosmic_CERNQC8_"$MODTIME"_"$ls"_"$index
-	echo $name
-	scp "gemuser@gem904qc8daq:$f" $DIRECTORY/$name
+((COUNT++))
+echo "Processing lumi section: " $l
+j=$(printf "%04d" $l)
+name=run${RUN}"_Cosmic_CERNQC8_"${MODTIME}"_ls"${j}"_allindex.raw"
+ssh gemuser@gem904qc8daq cat ${src}"_ls"${j}"_index"*".raw" >> $DIRECTORY/$name
+echo 'Processed lumi section' $l
+echo $name
 done
+
 
 #Confirm for the user that the files were copied
 echo "Showing the contents of " $DIRECTORY
 ls -1 | wc -l $DIRECTORY"/"*".raw"
 COPIED=$(ls $DIRECTORY)
 
+
 for c in $COPIED;
 do
-	((COPY++))
+((COPY++))
 done
 
 if [ $COUNT -eq $COPY ]; then
-        echo "All files have been copied!"
+echo "All files have been copied!"
 else
-	echo "Some files are missing!"
+echo "Some files are missing!"
 fi
