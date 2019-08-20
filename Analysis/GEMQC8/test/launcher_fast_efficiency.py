29874109cd19e3ea9f51de60ac860ddf61c1b2c3
Giovanni Mocellin <gio.moc@gmail.com>
2019-08-20 18:36:46 +0200

    Fast eff launcher with arg parser

--- Analysis/GEMQC8/test/launcher_fast_efficiency.py
+++ Analysis/GEMQC8/test/launcher_fast_efficiency.py
@@ -8,7 +8,12 @@ import datetime
 
 if __name__ == '__main__':
 
-    run_number = sys.argv[1]
+    # Define the parser
+    import argparse
+    parser = argparse.ArgumentParser(description="QC8 data analysis step 3. Fast efficiency calculation. For any doubt: https://twiki.cern.ch/twiki/bin/view/CMS/GEMCosmicRayAnalysis")
+    # Positional arguments
+    parser.add_argument("run_number", type=int, help="Specify the run number")
+    args = parser.parse_args()
 
     # Different paths definition
     srcPath = os.path.abspath("launcher_fast_efficiency.py").split('QC8Test')[0]+'QC8Test/src/'
@@ -25,7 +30,7 @@ if __name__ == '__main__':
     import geometry_files_creator
 
     # Retrieve start date and time of the run
-    fpath =  "/eos/cms/store/group/dpg_gem/comm_gem/QC8_Commissioning/run{:06d}/".format(int(run_number))
+    fpath =  "/eos/cms/store/group/dpg_gem/comm_gem/QC8_Commissioning/run{:06d}/".format(int(args.run_number))
     for x in os.listdir(fpath):
         if x.endswith("ls0001_allindex.raw"):
             file0name = x
@@ -39,15 +44,15 @@ if __name__ == '__main__':
     time.sleep(1)
 
     # Get stand configuration table from the DB
-    if int(run_number) >= 224:
-        dumpDBtables.getConfigurationTable(run_number,startDateTime)
+    if int(args.run_number) >= 224:
+        dumpDBtables.getConfigurationTable(args.run_number,startDateTime)
 
     # Generate configuration file
-    config_creator.configMaker(run_number)
+    config_creator.configMaker(args.run_number)
     time.sleep(1)
 
     # Generate geometry files
-    geometry_files_creator.geomMaker(run_number,"--noAlignment")
+    geometry_files_creator.geomMaker(args.run_number,"--noAlignment")
     time.sleep(1)
 
     # Compiling after the generation of the geometry files
@@ -71,7 +76,7 @@ if __name__ == '__main__':
     time.sleep(1)
 
     #  # Creating folder outside the CMMSW release to put the output files and plots
-    outDirName = "Results_QC8_fast_efficiency_run_"+run_number
+    outDirName = "Results_QC8_fast_efficiency_run_"+args.run_number
     #---# Remove old version if want to recreate
     if (os.path.exists(resDirPath+outDirName)):
         rmDirCommand = "rm -rf "+outDirName
@@ -96,9 +101,9 @@ if __name__ == '__main__':
 
     # Selecting the correct output file, changing the name and moving to the output folder
     out_name = 'out_run_'
-    for i in range(6-len(run_number)):
+    for i in range(6-len(args.run_number)):
         out_name = out_name + '0'
-    out_name = out_name + run_number + '.root'
+    out_name = out_name + args.run_number + '.root'
 
     mvToDirCommand = "mv fast_efficiency_" + out_name + " " + resDirPath+outDirName + "/fast_efficiency_" + out_name
     movingToDir = subprocess.Popen(mvToDirCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=runPath)
@@ -106,7 +111,7 @@ if __name__ == '__main__':
     time.sleep(1)
 
     # Efficiency computation & output
-    effCommand = "root -l -q " + runPath + "macro_fast_efficiency.c(" + run_number + ",\"" + configTablesPath + "\")"
+    effCommand = "root -l -q " + runPath + "macro_fast_efficiency.c(" + args.run_number + ",\"" + configTablesPath + "\")"
     efficiency = subprocess.Popen(effCommand.split(),stdout=subprocess.PIPE,universal_newlines=True,cwd=effoutDir)
     while efficiency.poll() is None:
         line = efficiency.stdout.readline()
